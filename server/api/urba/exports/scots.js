f10dbb094179d3bcb2db1921e272344e0dd85293
Anthony Ricaud <anthony@ricaud.me>
2025-01-19 20:59:20 +0000

    API SCoT - Regroupement des procédures par collectivité porteuse
    fix https://github.com/MTES-MCT/Docurba/issues/964

--- server/api/urba/exports/scots.js
+++ server/api/urba/exports/scots.js
@@ -1,19 +1,32 @@
-import { AsyncParser } from '@json2csv/node';
+import { AsyncParser } from '@json2csv/node'
 import _ from 'lodash'
-const csvParser = new AsyncParser();
+const csvParser = new AsyncParser()
 
 export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+
+  const status = []
+
+  if (query.pa === 'true') status.push('opposable')
+  if (query.pc === 'true') status.push('en cours')
+
+  const defaultStatus = ['opposable', 'en cours']
+  const queryStatus = status.length ? status : defaultStatus
+
   try {
     const scots = await getProcedures({
       doc_type: 'SCOT',
       is_principale: true,
       archived: false,
-      // id: '72420405-f1cc-4c8d-a526-1e73a9e74d81'
-    })
+    }, [
+      ['in', 'status', queryStatus]
+    ])
 
     const groupedScots = _.groupBy(scots, 'collectivite_porteuse_id')
 
-    const collectivites = _.map(groupedScots, (procedures) => {
+    const exportedScots = []
+
+    _.forEach(groupedScots, (procedures) => {
       const proceduresByStatus = _.groupBy(procedures, 'status')
 
       procedures.forEach(procedure => {
@@ -26,23 +39,48 @@ export default defineEventHandler(async (event) => {
         })
       }) 
 
-      const opposables = sortProceduresByEvenCateg(proceduresByStatus['opposable'] || [])
-      const currents = sortProceduresByEvenCateg((proceduresByStatus['en cours'] || []).filter((p) => {
-        return p.from_sudocuh ? !!p.prescription : true
-      }))
+      const opposables = sortProceduresByEvenCateg(proceduresByStatus['opposable'] || [], 'prescription').filter(p => {
+        // Cela enlève les procédures précédentes.
+        return !!p.procedures_perimetres.find(c => c.opposable)
+      })
 
-      return Object.assign({
-        cog: '2024',
-        opposables,
-        currents,
-        opposable: opposables[0],
-        current: currents[0]
-      }, procedures[0].collectivite)
+      const currents = sortProceduresByEvenCateg(proceduresByStatus['en cours'] || [], 'prescription').filter((p) => {
+        return p.from_sudocuh ? !!p.prescription : true
+      })
+
+      // Cette situation ne doit pas exister
+      if(currents.length > 1 && opposables > 1) {
+        throw new Error('multiple current et opposables' + currents[0].collectivite.code)
+      }
+
+      // We want to avoid duplicated lines. So we only do multiple lines for either opposable or currents.
+      // if you could have both multiple opposable and currents, it seems impossible to determine the correct combinaison.
+      if(opposables.length >= 1) {
+        opposables.forEach(p => {
+          exportedScots.push(Object.assign({
+            cog: '2024',
+            opposables,
+            currents,
+            opposable: p,
+            current: currents[0]
+          }, p.collectivite))
+        })
+      } else {
+        currents.forEach(p => {
+          exportedScots.push(Object.assign({
+            cog: '2024',
+            opposables,
+            currents,
+            opposable: opposables[0],
+            current: p
+          }, p.collectivite))
+        })
+      }
     })
 
     const sudocuhScots = await useStorage('assets:server').getItem(`/exportMaps/sudocuhScots.json`)
 
-    const mapedCollectivites = collectivites.map((c) => {
+    const mapedCollectivites = exportedScots.map((c) => {
       let scot = _.mapValues(sudocuhScots, key => _.get(c, key, ''))
 
       return scot
