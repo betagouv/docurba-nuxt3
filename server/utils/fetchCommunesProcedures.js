7d035e9b137dea6a833d466e43fc667e3b07f691
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2026-08-04 15:12:54 +0200

    Remove call to procedures_principales_by_collectivites

--- server/utils/fetchCommunesProcedures.js
+++ server/utils/fetchCommunesProcedures.js
@@ -10,29 +10,41 @@ function sortEvents(events) {
 export default async function (inseeCodes) {
   const time = Date.now()
 
-  const { data: proceduresRes } = await supabase.rpc('procedures_principales_by_collectivites', {
-    codes: inseeCodes
-  })
-
-  const procedures = proceduresRes.filter(p => p.doc_type !== 'SD')
+  const { data: procedures } = await supabase
+    .from("procedures")
+    .select("*, procedures_perimetres!inner ()")
+    .eq('is_principale', true)
+    .neq('doc_type', 'SD')
+    .in('status', ['opposable', 'en cours'])
+    .in('procedures_perimetres.collectivite_code', inseeCodes)
+    .throwOnError()
 
   const procedures_ids = procedures.map(p => p.id)
 
-  const { data: events } = await supabase
+  let { data: perimetres } = await supabase
+    .from("procedures_perimetres")
+    .select()
+    .in('procedure_id', procedures_ids)
+    .throwOnError()
+  
+  perimetres = _.groupBy(perimetres, perimetre => {
+    return perimetre.procedure_id
+  })
+
+  let { data: events } = await supabase
     .from("doc_frise_events")
     .select()
     .in('procedure_id', procedures_ids)
     .throwOnError()
 
-  const groupedEvents = _.groupBy(events, event => {
+  events = _.groupBy(events, event => {
     event.timestamp = +dayjs(event.date_iso)
     return event.procedure_id
   })
 
-  // console.log('Nb procedures with event', Object.keys(groupedEvents).length)
-
   procedures.forEach(p => {
-    p.events = sortEvents(groupedEvents[p.id] || [])
+    p.events = sortEvents(events[p.id] || []) // left join doc_frise_events e on e.procedure_id = procedure.id
+    p.procedures_perimetres = perimetres[p.id] || [] // left join procedures_perimetres pp on pp.procedure_id = procedure.id
   })
 
   console.log('fetched procedures in', (Date.now() - time) / 1000)
